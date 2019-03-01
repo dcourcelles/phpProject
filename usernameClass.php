5784b4e3472a16f24d313c9b3ce95e8418576d95
Dan Courcelles <39842100+dcourcelles@users.noreply.github.com>
2019-03-01 08:55:13 -0600

    Update usernameClass.php

--- usernameClass.php
+++ usernameClass.php
@@ -2,7 +2,7 @@
   class login{
     private $server = "localhost";
     private $username = "root";
-    private $password = "lothric90";
+    private $password = "";
     private $dbname = "dbDansCoffeeShop";
     public $db_conn;
 
